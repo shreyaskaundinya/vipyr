fa58f6c5ee3abc199ec4f2aa768d302a443984bf
Shreyas Kaundinya <shreyassk08@gmail.com>
2022-07-13 16:07:02 +0530

    Fixed dictionary, function updates, array updates of state; included test file

--- src/state.py
+++ src/state.py
@@ -1,17 +1,38 @@
 from js import console
+from copy import deepcopy
 
 class State:
     def __init__(self, initialState, dispatcher):
         self.state = initialState
         self.dispatcher = dispatcher
-        # console.log("called init with ", initialState)
     
     def set(self, action):
-        # console.log("CALLING STATE SET")
+        """
+            Setter function for state
+        """
+        
+        """
+            Cases : 
+                Based on state type:
+                    - dict
+                    - list
+                    - set
+                    - tuple -> immutable
+                    - number, string, float, bool -> immutable
+                    - others
+                Based on action type:
+                    - function action
+                    - dictionary update
+                    - value update (immutable)
+                    - array
+        """
+        
+        # if action is a function
         if hasattr(action, "__call__"):
-            self.state = action(self.state)
-
-        if type(action) is dict and type(self.state) is dict:
+            self.state = action(deepcopy(self.state))
+        
+        # if action is a dictionary
+        elif type(action) is dict and type(self.state) is dict:
             for key in action.keys():
                 self.state[key] = action[key]
         else:
@@ -20,6 +41,11 @@ class State:
         # console.log("called set with", self.state)
         self.dispatcher()
 
-    def get(self):
-        # console.log("called get => ", self.state)
-        return self.state
+    def get(self, key = None):
+        """
+            Getter function for state
+        """
+        if key and type(self.state) is dict and key in self.state.keys():
+            return deepcopy(self.state[key])
+        
+        return deepcopy(self.state)
